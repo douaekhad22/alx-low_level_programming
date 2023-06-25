145f928a4e68479f042f7ecd12c9769af477f3f4
douaekhad22 <douaekhaddor@gmail.com>
2023-06-25 12:00:29 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
 *main - main entry point
-*return : always 0 (success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+/**
+*return value of 'main' : always 0 (success)
+*/
 }
