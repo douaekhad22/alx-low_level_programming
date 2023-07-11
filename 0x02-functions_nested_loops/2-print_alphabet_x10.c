756c223a0bd055b65298e0ce13f863504f90ad97
douaekhad12 <douaefstt@gmail.com>
2023-07-11 12:16:21 -0700

    #include main.h
    /**
     * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
     * followed by a new line
     */
    void print_alphabet_x10(void)
    {
    char ch;
    int i;
    
    i = 0;
    
    while (i < 10)
    {
    ch = 'a';
    while (ch <= 'z')
    {
    _putchar(ch);
    ch++;
    }
    _putchar('\n');
    i++;
    }
    }

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,18 @@
  */
 void print_alphabet_x10(void)
 {
-	char SC;
+	char ch;
 	int i;
-	i=0
 
-	while (i<10)
+	i = 0;
+
+	while (i < 10)
 	{
-		for (SC='a'; SC<='z')
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(SC);
-			SC++;
+			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
 		i++;
