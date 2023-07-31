be7c6a215ea860e9c65ed9c69a5f556b92ad9b1d
douaekhad22 <douaekhaddor@gmail.com>
2023-07-31 15:50:51 -0700

    101 listint

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,56 +1,65 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint_safe - function that prints a listint_t linked list.
- * @head: pointer to head of a list.
+ * free_listp - frees a linked list
+ * @head: head of a list.
  *
- * Return: Length of list (INT)
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *s = head ,*f = head;
-	size_t ele = 0;
-	int is_loop = 0;
+	size_t nnodes = 0;
 
-	while (s && f && f->next)
-	{
-		if (!(f->next->next))
-			break;
-		s = s->next;
-		f = f->next->next;
-		if (s == f)
-		{
-			s = s->next;
-			is_loop = 1;
-			break;
-		}
-	}
+	listp_t *hptr, *new, *add;
 
-	if (!is_loop)
+	hptr = NULL;
+	while (head != NULL)
 	{
-		while (head)
-		{
-			ele++;
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-		return (ele);
-	}
-
-	while (head)
-	{
-		ele++;
-		if (head == s)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			printf("-> [%p] %d\n", (void *)head, head->next->n);
+		new = malloc(sizeof(listp_t));
+		if (new == NULL)
 			exit(98);
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+		add = hptr;
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
 		}
-
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
+		nnodes++;
 	}
-	return (0);
+	free_listp(&hptr);
+	return (nnodes);
 }
 
