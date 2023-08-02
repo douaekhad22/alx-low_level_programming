f13ce99e0538af7be42df6199145179f8ca873bd
douaekhad22 <douaekhaddor@gmail.com>
2023-08-01 19:20:28 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,6 @@
+#include <stdio.h>
 #include "lists.h"
 #include <stddef.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
 
 /**
  * print_listint - prints a singly linked list
@@ -25,10 +14,8 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		_putchar(h->n + '0');
-		_putchar('\n');
 		h = h->next;
 		size++;
 	}
-	return size;
+	return (size);
 }
